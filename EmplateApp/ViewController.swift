55ada203c2625decf00baf1956a7628c7d3ca299
Abanoub Nabil <abanoubn91@gmail.com>
2019-12-31 00:49:28 +0200

    add .git ignore + pods init

---
+++ EmplateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmplateApp
+//
+//  Created by Ahmed El-Diftar on 12/31/19.
+//  Copyright © 2019 Crossworkers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
